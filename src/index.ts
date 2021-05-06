51e59807b58c1f6c3c6757d5a7f17bbf87cb3b83
Sam <sam.hatem17@gmail.com>
2021-05-06 13:48:10 -0400

    fix: export JsonRpcFetchFunc type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { JsonRpcMultiProvider } from "./JsonRpcMultiProvider";
-import { Web3MultiProvider } from "./Web3MultiProvider";
+import { Web3MultiProvider, JsonRpcFetchFunc } from "./Web3MultiProvider";
 
 export { JsonRpcMultiProvider, Web3MultiProvider };
+export type { JsonRpcFetchFunc };
